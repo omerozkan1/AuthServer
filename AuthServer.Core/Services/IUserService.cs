8315373b87fef2c9b91e6971b76f6bff04c3ba6c
omer <gs_omer_ozkan@hotmail.com>
2021-01-28 13:52:03 +0300

    IUserService and IUnitOfWork interfaces added.

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.DTO;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<AppUserDTO>> CreateUserAsync(CreateUserDTO user);
+        Task<Response<AppUserDTO>> GetUserByNameAsync(string userName);
+    }
+}
